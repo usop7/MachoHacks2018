a052ccde0153494a21b670405643835e837feed7
Leeseul Kim <dewkim7@gmail.com>
2018-07-14 21:50:25 -0700

    created post and some css

---
+++ post.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>BCIT Transcript Admin</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <h1>Inserting transcript data</h1>
+    <br>
+    <form action='create_process.php' method='post'>
+      <p><input type='text' name='number' placeholder='Student Number'></p>
+      <p><input type='text' name='name' placeholder='Student Name'></p>
+      <p><input type='text' name='term' placeholder='Term'></p>
+      <p><input type='text' name='data' placeholder='Grade Number'></p>
+      
+      <input type='submit' name='Submit'>
+    </form>
+
+
+  </body>
+</html>
